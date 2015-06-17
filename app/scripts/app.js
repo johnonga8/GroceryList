078ad42b51d49cb9a3847e6bb51bd99875f08a6e
John Ong <john.ong@a8.sg>
2015-06-17 07:16:04 +0000

    Changes made by John Ong (john.ong@a8.sg).

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,13 +1,12 @@
 (function() {
-    var apiKey = "YOUR API KEY";
+    var apiKey = "e6DOv4wQZ38bPafV";
     var el = new Everlive(apiKey);
 
     var groceryDataSource = new kendo.data.DataSource({
+        type: "everlive",
+        sort:{field: "Name", dir:"asc"},
         transport: {
-            read: {
-                url: "https://api.everlive.com/v1/" + apiKey + "/Groceries",
-                dataType: "jsonp"
-            }
+        	typeName: "Groceries"
         },
         schema: {
             data: function(response) {
@@ -21,8 +20,97 @@
             skin: "flat",
             transition: "slide"
         });
+        $("#grocery-list").kendoMobileListView({
+            dataSource: groceryDataSource,
+            template: "#: Name #"
+        });
         navigator.splashscreen.hide();
     }
+    window.loginView = kendo.observable({
+        submit: function(){
+            if(!this.username){
+                navigator.notification.alert("Username is required.");
+                return;
+            }
+            if(!this.password){
+                navigator.notification.alert("Password is required.");
+                return;
+            }
+            el.Users.login(this.username, this.password, function(data){
+                window.location.href = "#list";
+                groceryDataSource.read();
+            },function(){
+                navigator.notification.alert("You don't have an account.");
+            });
+        }
+    });
+    window.registerView = kendo.observable({
+        submit: function(){
+            if(!this.username){
+                navigator.notification.alert("Username is required.");
+                return;
+            }
+            if(!this.password){
+                navigator.notification.alert("Password is required.");
+                return;
+            }
+            el.Users.register(this.username, this.password,{Email: this.email}, function(){
+                navigator.notification.alert("Your account was successfully created.");
+                window.location.href = "#login";
+            }, function(){
+                navigator.notification.alert("Unfortunately, we were unable to create your account.");
+            });
+        }
+    });
+    window.passwordView = kendo.observable({
+        submit: function(){
+            if(!this.email){
+                navigator.notification.alert("Email address is required.");
+                
+            }
+         $.ajax({
+                  type: "POST",
+                  url: "https://api.everlive.com/v1/" + apiKey + "/Users/resetpassword",
+                  contentType: "application/json",
+                  data: JSON.stringify({ Email: this.email }),
+                  success: function() {
+                      navigator.notification.alert("Your password was successfully reset. Please check your email for instructions on choosing a new password.");
+                      window.location.href = "#login";
+                  },
+                  error: function() {
+                      navigator.notification.alert("Unfortunately, an error occurred resetting your password.")
+                  }
+              });
+          }
+    });
+    window.listView = kendo.observable({
+        logout: function(event){
+            // Prevent going to the login page until the login call processes.
+            event.preventDefault();
+            el.Users.logoutfunction(function(){
+                this.loginView.set("username", "");
+                this.loginView.set("password", "");
+                window.location.href = "#login";
+            },function(){
+                navigator.notification.alert("Error while logging out");
+            });
+        }
+    });
+    window.addView = kendo.observable({
+        add: function(){
+            if (!this.grocery){
+                navigator.notification.alert("Please provide a grocery, dummy.");
+                return;
+            }
+            groceryDataSource.add({Name: this.grocery});
+            groceryDataSource.one("sync", this.close);
+            groceryDataSource.sync();
+        },
+        close: function(){
+            $("#add").data("kendoMobileModalView").close();
+            this.grocery = "";
+        }
+    });
 
     document.addEventListener("deviceready", initialize);
 }());
